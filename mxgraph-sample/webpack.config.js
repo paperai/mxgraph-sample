2e3693682e13df43e8e3ee5470ad3ca393cc7a7f
Toshiyuki Yamamoto <kikipeco22@gmail.com>
2018-08-31 20:51:17 +0900

    webpackを使用します

---
+++ mxgraph-sample/webpack.config.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = (env, argv) => {
+  return {
+    mode: process.env.NODE_ENV || 'development',
+    entry: {
+      'index': 'index.js'
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: './[name].bundle.js'
+    },
+    module: {
+    },
+    plugins: [
+    ],
+    devtool: 'source-map'
+  }
+}
